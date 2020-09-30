3a962c8ff24fccb00bfc74ed0aa1464686d87014
Amin Ghasembeigi <me@aminbeigi.com>
2020-09-30 23:49:42 +1000

    Add all spec functions

--- src/Library-Management-System/borrower.cpp
+++ src/Library-Management-System/borrower.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string>
 
 #include "borrower.h"
 
 Borrower::Borrower() {
-	std::cout << "This is the epic borrower class.";
+	std::cout << "Epic constructor" << std::endl;
+}
+
+void Borrower::set_id(int id) {
+	this->id = id;
+}
+
+void Borrower::set_name(std::string name) {
+	this->name = name;
+}
+
+int Borrower::get_id() {
+	return this->id;
+}
+
+std::string Borrower::get_name() {
+	return this->name;
 }
--- src/Library-Management-System/borrower.h
+++ src/Library-Management-System/borrower.h
@@ -1,10 +1,19 @@
 #ifndef GUARD_BORROWER_H
 #define GUARD_BORROWER_H
 
+#include <string>
+
 class Borrower {
 public:
 	Borrower();
-
+	void set_id(int id);
+	void set_name(std::string name);
+	int get_id();
+	std::string get_name();
+	
+private:
+	std::string name;
+	int id = 0;
 };
 
 #endif
