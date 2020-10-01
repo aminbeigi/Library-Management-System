eac95da0ee9aef41669fd03df92f2426e7f9ea1f
Amin Ghasembeigi <me@aminbeigi.com>
2020-10-01 14:56:46 +1000

    Add all spec functions

--- src/Library-Management-System/book.cpp
+++ src/Library-Management-System/book.cpp
@@ -1,1 +1,46 @@
 #include "book.h"
+
+// global variable
+unsigned int book_count = 0;
+
+Book::Book() {
+	++book_count;
+}
+
+void Book::set_item_number(std::string item_number) {
+	this->item_number = item_number;
+}
+
+void Book::set_author(std::string author) {
+	this->author = author;
+}
+
+void Book::set_title(std::string title) {
+	this->title = title;
+}
+
+// current date and time on the current system
+void Book::set_date_borrowed(time_t date_borrowed) {
+	this->date_borrowed = time(0);
+}
+
+std::string Book::get_item_number() {
+	return this->item_number;
+}
+
+std::string Book::get_author() {
+	return this->author;
+}
+
+std::string Book::get_title() {
+	return this->title;
+}
+
+// convert time_t object to string form
+char* Book::get_date_borrowed() {
+	return ctime(&date_borrowed);
+}
+
+unsigned int get_book_count() {
+	return book_count;
+}
--- src/Library-Management-System/book.h
+++ src/Library-Management-System/book.h
@@ -1,8 +1,28 @@
 #ifndef GUARD_BOOK_H
 #define GUARD_BOOK_H
 
-class Book {
+#include <string>
+#include <ctime>
 
+class Book {
+public:
+	Book();
+	void set_item_number(std::string item_number);
+	void set_author(std::string author);
+	void set_title(std::string title);
+	void set_date_borrowed(time_t date_borrowed);
+	std::string get_item_number();
+	std::string get_author();
+	std::string get_title();
+	char* get_date_borrowed();
+	unsigned int get_book_count();
+
+private:
+	std::string item_number;
+	std::string author;
+	std::string title;
+	time_t date_borrowed;
+	time_t date_returned;
 };
 
 #endif
