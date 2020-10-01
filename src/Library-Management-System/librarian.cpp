be02c698823ce232b151d70b4edfcafef3ab5f3d
Amin Ghasembeigi <me@aminbeigi.com>
2020-10-02 09:50:42 +1000

    Add all spec functions

--- src/Library-Management-System/librarian.cpp
+++ src/Library-Management-System/librarian.cpp
@@ -1,1 +1,20 @@
 #include "librarian.h"
+
+Librarian::Librarian() {
+}
+
+void Librarian::set_id(unsigned int id) {
+	this->id = id;
+}
+
+void Librarian::set_name(std::string name) {
+	this->name = name;
+}
+
+unsigned int Librarian::get_id() {
+	return this->id;
+}
+
+std::string Librarian::get_name() {
+	return this->name;
+}
--- src/Library-Management-System/librarian.h
+++ src/Library-Management-System/librarian.h
@@ -1,8 +1,18 @@
 #ifndef GUARD_LIBRARIAN_H
 #define GUARD_LIBRARIAN_H
 
-class Librarian {
+#include <string>
 
+class Librarian {
+public:
+	Librarian();
+	void set_id(unsigned int id);
+	void set_name(std::string name);
+	unsigned int get_id();
+	std::string get_name();
+private:
+	unsigned int id = 0;
+	std::string name;
 };
 
 #endif
