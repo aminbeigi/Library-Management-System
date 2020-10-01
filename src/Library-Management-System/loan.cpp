52b8e02478a8360d23c53f99f0c18feaf4d4eead
Amin Ghasembeigi <me@aminbeigi.com>
2020-10-02 09:39:50 +1000

    Initial commit

--- src/Library-Management-System/loan.cpp
+++ src/Library-Management-System/loan.cpp
@@ -1,1 +1,12 @@
 #include "loan.h"
+
+Loan::Loan() {
+}
+
+void Loan::set_number_of_books(unsigned int number_of_books) {
+	this->number_of_books = number_of_books;
+}
+
+unsigned int Loan::get_number_of_books() {
+	return number_of_books;
+}
--- src/Library-Management-System/loan.h
+++ src/Library-Management-System/loan.h
@@ -2,6 +2,12 @@
 #define GUARD_LOAN_H
 
 class Loan {
+public:
+	Loan();
+	void set_number_of_books(unsigned int number_of_books);
+	unsigned int get_number_of_books();
+private:
+	unsigned int number_of_books = 0;
 };
 
 #endif
