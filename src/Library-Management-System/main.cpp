07df807e96e9d8d82d7893d39a04e0a31ae40651
Amin Ghasembeigi <me@aminbeigi.com>
2020-10-02 12:00:57 +1000

    Fix UI loop:

--- src/Library-Management-System/main.cpp
+++ src/Library-Management-System/main.cpp
@@ -15,6 +15,12 @@ int main() {
 	Book* book2 = new Book();
 	Book* book3 = new Book();
 
+	borrower->set_id(10000);
+	borrower->set_name("George Bush");
+
+	librarian1->set_id(10);
+	librarian1->set_name("Alyssa Bray");
+
 	book1->set_item_number("100");
 	book1->set_author("J. K. Rowling");
 	book1->set_title("Harry Potter and the Sorcerer's Stone");
@@ -27,34 +33,39 @@ int main() {
 
 	std::list<Book> books = { *book1, *book2, *book3 };
 
-	librarian1->set_id(10);
-	librarian1->set_name("Alyssa Bray");
 
-	std::cout << "Welcome to the Library" << std::endl;
+	std::cout << "Hello " << borrower->get_name() << ", welcome to the Library!" << std::endl;
 	std::cout << "There are " << book1->get_book_count() << " books to choose from." << std::endl;
-	std::cout << "from the following select an option (e.g. '1'): " << std::endl;
 	
 	int user_input;
 	while (true) {
-		std::cout << std::string(2, '\n'); // clear screen
-		std::cout << "1) List all book names." << std::endl;
+		std::cout << std::string(3, '\n'); // clear screen
+		std::cout << "1) Select a book to borrow." << std::endl;
 		std::cout << "2) exit" << std::endl;
 		std::cout << "Input: ";
 		std::cin >> user_input;
+		std::cout << std::endl;
 
+		int i = 1;
 		switch (user_input) {
 		case 1: 
-			for (std::list<Book>::iterator it = books.begin(); it != books.end(); ++it) {
-				std::cout << "- " << it->get_title() << ", by ";
-				std::cout << it->get_author() << std::endl;
-			}
+			std::cout << "Book names:" << std::endl;
 
+			for (std::list<Book>::iterator it = books.begin(); it != books.end(); ++it) {
+				std::cout << '\t' << i << ") " << it->get_title() << ", by ";
+				std::cout << it->get_author() << std::endl;
+				++i;
+			}
+			std::cout << "Input: ";
+			
 			break;
+
 		case 2:
 			std::cout << "Exiting the program..." << std::endl;
 			return 0;
+
 		default:
-			std::cout << "Incorrect input try again" << std::endl;
+			std::cout << "Incorrect input. From the following select an option (e.g. '1'). " << std::endl;
 			break;
 		}
 
